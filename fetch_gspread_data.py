f2220892c6bb3da47cadeefb4257fd36d93a2195
ugrbeytekin <ugrbeytekin@gmail.com>
2024-08-16 02:59:15 +0300

    Update fetch_gspread_data.py

--- fetch_gspread_data.py
+++ fetch_gspread_data.py
@@ -14,4 +14,16 @@ client = gspread.authorize(creds)
 # The ID of your spreadsheet
 spreadsheet_id = '1PxIp-ggZ6l1QXLQ9wPCO1nbXA2ZUb9nRzzzGOtOJ0Ks'
 
-# Your existing data fetching code here...
+# Your data fetching code here...
+
+# Debugging: Print the current working directory
+print("Current working directory:", os.getcwd())
+
+# Write the JSON file in the current directory
+json_file_path = os.path.join(os.getcwd(), 'forbes_blog_data.json')
+
+# Assuming json_data is the data you want to write
+with open(json_file_path, 'w') as json_file:
+    json.dump(json_data, json_file, indent=2)
+
+print(f"JSON data saved to {json_file_path}")
